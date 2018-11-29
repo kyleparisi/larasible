9ef4f0b7e2df23d0f2a0f45fa0a1c5d5219e8f6b
Kyle Parisi <kyleparisi@gmail.com>
2018-11-29 08:26:51 -0500

    Update to ubuntu bionic Link php-fpm to the proper version

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /etc/init.d/nginx start
-/etc/init.d/php7.2-fpm start || /etc/init.d/php7.1-fpm start
+/etc/init.d/php-fpm start start
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log /var/www/default/storage/logs/laravel.log
