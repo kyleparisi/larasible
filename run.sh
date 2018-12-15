6cb99f48759063ebc8828060214f455882943165
Kyle Parisi <kyleparisi@gmail.com>
2018-12-15 07:42:34 -0500

    remove typo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /etc/init.d/nginx start
-/etc/init.d/php-fpm start start
+/etc/init.d/php-fpm start
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log /var/www/default/storage/logs/laravel.log
