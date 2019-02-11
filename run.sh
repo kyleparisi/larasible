5f21fd5e17bbfa25fd6f16354cca0e1f9c1859ee
Ravi Gehlot <ravi@gehlot.net>
2019-02-11 17:48:47 -0500

    Fixed issue with php-fpm not starting

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Get the current PHP version
+current_version=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3)
+formatted_version=${current_version:0:1}.${current_version:1:2}
+
 /etc/init.d/nginx start
+cp -rf /etc/init.d/php${formatted_version}-fpm /etc/init.d/php-fpm 
 /etc/init.d/php-fpm start
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log /var/www/default/storage/logs/laravel.log
