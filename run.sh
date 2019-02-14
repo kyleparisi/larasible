a05e77510a716c8ceab6f77a6d50c3abbfbd51c2
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-02-14 14:47:31 -0500

    Handle ansible signature warning (+3 squashed commits) Squashed commits: [78ead41] Accept any log in storage/logs [bea5eb7] Remove 403 for no user agent request [f9b19d0] Use bionic repo name

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 /etc/init.d/nginx start
 /etc/init.d/php-fpm start
-tail -f /var/log/nginx/access.log /var/log/nginx/error.log /var/www/default/storage/logs/laravel.log
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log /var/www/default/storage/logs/*.log
